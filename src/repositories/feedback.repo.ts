9336e7c495621fced2f7881e095edcfa10b48393
WilsonnnTan <wilsonangelietann@gmail.com>
2026-03-12 13:59:52 +0700

    refactor: select specific fields and include user name in feedback prisma queries

--- src/repositories/feedback.repo.ts
+++ src/repositories/feedback.repo.ts
@@ -2,13 +2,30 @@ import prisma from '@/lib/prisma';
 
 const FeedbackRepository = {
   async getAllFeedback() {
-    return prisma.feedback.findMany();
+    return prisma.feedback.findMany({
+      select: {
+        user: {
+          select: { name: true },
+        },
+        id: true,
+        title: true,
+        category: true,
+      },
+    });
   },
 
   async getFeedbackByUserId(userId: string) {
     return prisma.feedback.findMany({
       where: {
         userId: userId,
+      },
+      select: {
+        user: {
+          select: { name: true },
+        },
+        id: true,
+        title: true,
+        category: true,
       },
     });
   },
